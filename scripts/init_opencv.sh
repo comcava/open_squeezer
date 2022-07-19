4471fa85d4cfc102972d738e7fe83e03f53ef97a
nWacky <gosha.evtushenko@gmail.com>
2022-07-19 19:08:03 +0300

    start adding cpp plugin

--- scripts/init_opencv.sh
+++ scripts/init_opencv.sh
@@ -3,14 +3,14 @@
 mkdir -p download
 cd download
 
-wget -O opencv-4.6.0-android-sdk.zip https://sourceforge.net/projects/opencvlibrary/files/4.6.0/opencv-4.6.0-android-sdk.zip/download
-wget -O opencv-4.6.0-ios-framework.zip https://sourceforge.net/projects/opencvlibrary/files/4.6.0/opencv-4.6.0-ios-framework.zip/download
+# wget -O opencv-4.6.0-android-sdk.zip https://sourceforge.net/projects/opencvlibrary/files/4.6.0/opencv-4.6.0-android-sdk.zip/download
+# wget -O opencv-4.6.0-ios-framework.zip https://sourceforge.net/projects/opencvlibrary/files/4.6.0/opencv-4.6.0-ios-framework.zip/download
 
-unzip opencv-4.5.4-android-sdk.zip
-unzip opencv-4.5.4-ios-framework.zip
+unzip opencv-4.6.0-android-sdk.zip
+unzip opencv-4.6.0-ios-framework.zip
 
-cp -r opencv2.framework ../../native_opencv/ios
-cp -r OpenCV-android-sdk/sdk/native/jni/include ../../native_opencv
-mkdir -p ../../native_opencv/android/src/main/jniLibs/
-cp -r OpenCV-android-sdk/sdk/native/libs/* ../../native_opencv/android/src/main/jniLibs/
+cp -r opencv2.framework ../../plugins/native_opencv/ios
+cp -r OpenCV-android-sdk/sdk/native/jni/include ../../plugins/native_opencv
+mkdir -p ../../plugins/native_opencv/android/src/main/jniLibs/
+cp -r OpenCV-android-sdk/sdk/native/libs/* ../../plugins/native_opencv/android/src/main/jniLibs/
 
