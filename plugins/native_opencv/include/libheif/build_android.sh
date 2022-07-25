14eb0f08a34242f92d00848a6cbe13dfad55dd24
nWacky <gosha.evtushenko@gmail.com>
2022-07-25 13:33:55 +0300

    try using a script

---
+++ plugins/native_opencv/include/libheif/build_android.sh
@@ -0,0 +1,15 @@
+NDK=$HOME/Library/Android/sdk/ndk/21.4.7075529
+
+for ABI in "arm64-v8a" "x86_64" "x86" "armeabi-v7a"; do
+
+    DESTINATION_DIR=../build-libheif/$ABI     
+    mkdir -p $DESTINATION_DIR
+
+    cmake -S . -B $DESTINATION_DIR \
+        -DCMAKE_TOOLCHAIN_FILE=$NDK/build/cmake/android.toolchain.cmake \
+        -DANDROID_ABI=$ABI \
+        -DANDROID_NATIVE_API_LEVEL=29
+
+    cmake --build $DESTINATION_DIR -j 4
+done
+
